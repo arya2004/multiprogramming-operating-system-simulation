637c73861edb5a985c69ac833a39162572bdabbc
arya2004 <arya20j4@gmail.com>
2023-08-14 06:26:57 +0530

    Add project files.

---
+++ OS_Phase_1/Models/CPU.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_Phase_1.Models
+{
+    public class CPU
+    {
+    }
+}
---
+++ OS_Phase_1/Models/ExternalMemory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_Phase_1.Models
+{
+   
+
+    public class ExternalMemory
+    {
+        public char[,] Memory { get; set; }
+        private int _Blocks { get; set; }
+        private int _WordLength { get; set; }
+        private int _WordsPerBlock { get; set; }
+
+        public ExternalMemory(int WordLength, int Blocks, int WordsPerBlock)
+        {
+            Memory = new char[WordsPerBlock * Blocks, WordLength];
+            _WordLength = WordLength;
+            _Blocks = Blocks;
+            _WordsPerBlock = WordsPerBlock;
+        }
+
+        public char[] GetWord(int Address)
+        {
+            char[] Word = new char[_WordLength];
+            for (int i = 0; i < _WordLength; i++)
+            {
+                Word[i] = Memory[Address, i];
+            }
+            return Word;
+        }
+
+        public char[,] GetBlock(int BlockAddress)
+        {
+            char[,] Block = new char[_WordsPerBlock, _WordLength];
+            //logic remaining
+            return Block;
+        }
+
+    }
+}

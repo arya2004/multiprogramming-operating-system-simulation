073a014fdfa46b968a83f05c0237e3b8f5d72468
arya2004 <arya20j4@gmail.com>
2023-08-21 11:33:28 +0530

    feat: io and buffer

--- OS_Phase_1/Models/IO.cs
+++ OS_Phase_1/Models/IO.cs
@@ -6,8 +6,52 @@ using System.Threading.Tasks;
 
 namespace OS_Phase_1.Models
 {
-    public class IO
+    public class INputOutput
     {
+        private char[] _Buffer;
+        private int _WordLength { get; set; }
+        private int _WordsPerBlock { get; set; }
+        public static readonly string CardReader = "C:\\Users\\arya2\\Documents\\OS_Coursse_Project\\OS_Phase_1\\IO_Files\\CArdReader.txt";
+        public static readonly string LinePrinters = "C:\\Users\\arya2\\Documents\\OS_Coursse_Project\\OS_Phase_1\\IO_Files\\LinePrinter.txt";
+        public INputOutput(int WordLength,  int WordsPerBlock)
+        {
+            _Buffer = new char[WordsPerBlock* WordLength];
+            _WordLength = WordLength;
+            _WordsPerBlock = WordsPerBlock;
+
+        }
+
+        public void WriteTOBuffer(char[,] Data)
+        {
+            int k = 0;
+            for (int i = 0; i < _WordsPerBlock; i++)
+            {
+                for (int j = 0; j < _WordLength; j++)
+                {
+                    _Buffer[k] = Data[i, j];
+                    k++;
+                }
+            }
+        }
+        
+
+        public void Write()
+        {
+            StreamWriter sw = new StreamWriter(LinePrinters);
+            for (int i = 0; i < _WordsPerBlock * _WordLength; i++)
+            {
+                sw.Write(_Buffer[i]);
+            }
+            sw.Close();
+        }
+
+        public void Read()
+        {
+            StreamReader sr = new StreamReader(CardReader);
+            String line = sr.ReadLine();
+            _Buffer = line.ToCharArray();
+            sr.Close();
+        }
 
     }
 }
--- OS_Phase_1/Program.cs
+++ OS_Phase_1/Program.cs
@@ -1,18 +1,32 @@
-﻿namespace OS_Phase_1
+﻿using OS_Phase_1.Models;
+
+namespace OS_Phase_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            StreamWriter sw = new StreamWriter("C:\\Users\\arya2\\Documents\\OS_Coursse_Project\\OS_Phase_1\\IO_Files\\INPUT.txt");
-            sw.WriteLine("Hello World!!");
-            sw.WriteLine("From the StreamWriter class");
-            sw.Close();
+            char[,] charMatrix = new char[,]
+        {
+            { 'A', 'B', 'C', 'D' },
+            { 'E', 'F', 'G', 'H' },
+            { 'I', 'J', 'K', 'L' },
+            { 'M', 'N', 'O', 'P' },
+            { 'Q', 'R', 'S', 'T' },
+            { 'U', 'V', 'W', 'X' },
+            { 'Y', 'Z', 'A', 'B' },
+            { 'C', 'D', 'E', 'F' },
+            { 'G', 'H', 'I', 'J' },
+            { 'K', 'L', 'M', 'N' }
+        };
+            INputOutput io = new INputOutput(4, 10);
+            io.WriteTOBuffer(charMatrix);
+            io.Write();
         }
     }
 }
 
-
+//"C:\\Users\\arya2\\Documents\\OS_Coursse_Project\\OS_Phase_1\\IO_Files\\INPUT.txt"
 //StreamReader sr = new StreamReader("C:\\Sample.txt");
 //line = sr.ReadLine();
 //while (line != null)
